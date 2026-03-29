066f8a0ab5b2bb9b418db56b6636ffa2b2e8a067
Neha <107941977+Nehaparmar1@users.noreply.github.com>
2026-03-29 16:17:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const result = document.getElementById('result');
+const enterButton = document.getElementById('enterButton');
+const mysteryImage = document.getElementById('mysteryImage');
+const guessInput = document.getElementById('guessInput');
+let answer = "Taylor Swift"
+let guessCounter = 0 
+let blurAmount = 10
+
+guessInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        enterButton.click();
+    }
+});
+
+enterButton.onclick = function()
+{
+    let userInput = document.getElementById('guessInput').value;
+    if(userInput.toLowerCase() != answer.toLowerCase())
+    {
+        guessInput.value = "";
+    }
+    if (userInput.toLowerCase() === answer.toLowerCase())
+    {
+        result.textContent = 'You guessed it!';
+        guessCounter++;
+    }
+    else
+    {
+        blurAmount -= 3.5;
+        mysteryImage.style.filter = "blur(" + blurAmount + "px)";
+        guessCounter++
+    }
+    
+    if (guessCounter === 3 && userInput.toLowerCase() !== answer.toLowerCase())
+    {
+        result.textContent = 'Out of guesses!';
+    }
+
+    if (guessCounter === 3 || userInput.toLowerCase() === answer.toLowerCase())
+    {
+        mysteryImage.style.filter = "   blur(0px)";
+        guessInput.disabled = true;
+        enterButton.disabled = true;
+
+    }
+}
+
+
+
+
