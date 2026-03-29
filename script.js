8a7daccbc0733096b80dd5b708fc69f650dde3f9
Neha <107941977+Nehaparmar1@users.noreply.github.com>
2026-03-29 19:45:52 -0400

    update

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let answer = "Taylor Swift"
 let guessCounter = 0 
 let blurAmount = 10
 
-console.log("new loaded")
 guessInput.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
         enterButton.click();
